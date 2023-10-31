d9855ff56ba861e1e300b120a19e508ca5a63f83
Hyungjun <pparkheong96@naver.com>
2023-10-31 16:09:38 +0900

    ex02 finish

--- cpp_module_00/ex02/Account.cpp
+++ cpp_module_00/ex02/Account.cpp
@@ -53,16 +53,34 @@ void Account::displayAccountsInfos(void) {
 void Account::makeDeposit(int deposit) {
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << deposit << ";amount:" << this->_amount + deposit << ";nb_deposits:" << this->_nbDeposits + 1 << std::endl;
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
 }
 
 bool Account::makeWithdrawal(int withdrawal) {
-
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_mount:" << this->_amount;
+	if (this->_amount < withdrawal) {
+		std::cout << ";withdrawal:refused" << std::endl;
+		return false;
+	}
+	else {
+		std::cout << ";withdrawal:"  << withdrawal << ";amount:" << this->_amount - withdrawal << ";nb_withdrawals:" << this->_nbWithdrawals + 1 << std::endl;
+		this->_amount -= withdrawal;
+		this->_nbWithdrawals++;
+		this->_totalAmount -= withdrawal;
+		this->_totalNbWithdrawals++;
+		return true;
+	}
 }
 
 int Account::checkAmount(void) const {
-
+	return this->_amount;
 }
 
 void Account::displayStatus(void) const {
-
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:"  << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
