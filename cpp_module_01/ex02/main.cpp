7790bbaf2d19deecc296785641a553c75ea706d1
Hyungjun <pparkheong96@naver.com>
2023-11-01 15:45:15 +0900

    module 01: ex03 완료

---
+++ cpp_module_01/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "string address: " << &string << std::endl;
+	std::cout << "stringPTR address: " << stringPTR << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
+
+	return (0);
+}
