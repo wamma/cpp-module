24a391fa1fcc4fdae3046020f38d0aa14c40fd11
Hyungjun <pparkheong96@naver.com>
2023-11-09 17:49:24 +0900

    module02: ex00 수정

--- cpp_module_02/ex00/Fixed.cpp
+++ cpp_module_02/ex00/Fixed.cpp
@@ -8,23 +8,24 @@ Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &source) : fixedPointValue(source.fixedPointValue) {
+Fixed::Fixed(const Fixed &source) {
 	std::cout << "Copy constructor called" << std::endl;
+	*this = source;
 }
 
 Fixed	&Fixed::operator=(const Fixed &source) {
 	std::cout << "Copy assignment operator called" << std::endl;
-	if (this == &source)
+	if (this == &source) // 자기 자신에 대한 할당을 방지하는 역할
 		return *this;
 	this->fixedPointValue = source.getRawBits();
 	return *this;
 }
 
-void Fixed::setRawBits(int const raw) {
-	this->fixedPointValue = raw;
-}
-
 int Fixed::getRawBits(void) const {
 	std::cout << "getRawbits member function called" << std::endl;
 	return this->fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw) {
+	this->fixedPointValue = raw;
 }
