93b806325eef792cb6ae7793c9986fe6e0dac05f
park hyung jun <55447614+wamma@users.noreply.github.com>
2023-11-14 14:43:51 +0900

    02: fix ex02

--- cpp_module_02/ex02/Fixed.cpp
+++ cpp_module_02/ex02/Fixed.cpp
@@ -91,19 +91,31 @@ Fixed Fixed::operator--(int) {
 }
 
 Fixed& Fixed::min(Fixed& a, Fixed& b) {
-	return (a < b) ? a : b;
+	if (a < b)
+		return a;
+	else
+		return b;
 }
 
 const Fixed& Fixed::min(const Fixed& a, const Fixed& b) {
-	return (a < b) ? a : b;
+	if (a < b)
+		return a;
+	else
+		return b;
 }
 
 Fixed& Fixed::max(Fixed& a, Fixed& b) {
-	return (a > b) ? a : b;
+	if (a > b)
+		return a;
+	else
+		return b;
 }
 
 const Fixed& Fixed::max(const Fixed& a, const Fixed& b) {
-	return (a > b) ? a : b;
+	if (a > b)
+		return a;
+	else
+		return b;
 }
 
 int Fixed::toInt() const {
