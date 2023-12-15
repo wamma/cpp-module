01764bc00e462339711e06e86befedefad8df834
park hyung jun <55447614+wamma@users.noreply.github.com>
2023-12-15 18:35:27 +0900

    04: fix Brain.cpp 수정

--- cpp_module_04/ex02/Brain.cpp
+++ cpp_module_04/ex02/Brain.cpp
@@ -2,6 +2,9 @@
 
 Brain::Brain() {
 	std::cout << "Brain 기본 생성자 호출" << std::endl;
+	for (int i = 0; i < 100; ++i) {
+		ideas[i] = "ideas";
+	}
 }
 
 Brain::Brain(const Brain& other) {
@@ -29,7 +32,8 @@ std::string Brain::getIdeas(int index) const {
 	if (index >= 0 && index < 100) {
 		return ideas[index];
 	}
-	return "";
+	std::cerr << "Error: " << index << " is out of range." << std::endl;
+	return ""; // 함수를 호출하는 쪽에서 잘못된 인덱스로 접근했음을 알리기 위해 빈 문자열을 반환한다.
 }
 
 void Brain::setIdeas(int index, const std::string& idea) {
