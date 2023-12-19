224b5f2b60b606ba8e5625238284829471ce48a4
park hyung jun <55447614+wamma@users.noreply.github.com>
2023-12-19 20:12:10 +0900

    add 06: ex01

---
+++ cpp_module_06/ex01/Serialization.hpp
@@ -0,0 +1,25 @@
+#ifndef SERIALIZATION_HPP
+# define SERIALIZATION_HPP
+
+# include <iostream>
+
+class Serialization
+{
+	private:
+		Serialization();
+		Serialization(const Serialization &copy);
+		Serialization &operator=(const Serialization &copy);
+
+	public:
+		~Serialization();
+		struct Data
+		{
+			std::string s1;
+			int n;
+			std::string s2;
+		};
+		uintptr_t serialize(Data* ptr);
+		Data* deserialize(uintptr_t raw);
+};
+
+#endif
